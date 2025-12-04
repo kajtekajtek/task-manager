6def0293afa6b7d6602137948e168a5c8ea9b379
kajtek <kajtek0123@gmail.com>
2025-12-04 16:10:25 +0100

    add bean, repo and entity scans to TaskManagerApp

--- task-manager-api/src/main/java/com/taskmanager/api/TaskManagerApplication.java
+++ task-manager-api/src/main/java/com/taskmanager/api/TaskManagerApplication.java
@@ -2,8 +2,12 @@ package com.taskmanager.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.taskmanager")
+@EnableJpaRepositories(basePackages = "com.taskmanager.repository")
+@EntityScan(basePackages = "com.taskmanager.model.entity")
 public class TaskManagerApplication {
 
     public static void main(String[] args) {
