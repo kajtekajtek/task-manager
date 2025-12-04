3187dcffdcd02f053af85833af9c534d1da88611
kajtek <kajtek0123@gmail.com>
2025-12-04 16:12:20 +0100

    add loggin gto CustomUserDetailsService

--- task-manager-service/src/main/java/com/taskmanager/service/CustomUserDetailsService.java
+++ task-manager-service/src/main/java/com/taskmanager/service/CustomUserDetailsService.java
@@ -3,11 +3,17 @@ package com.taskmanager.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.taskmanager.model.entity.User;
 import com.taskmanager.repository.UserRepository;
+
+import jakarta.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,7 +21,14 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UserRepository userRepository;
+    private final Logger LOGGER = LoggerFactory.getLogger(CustomUserDetailsService.class);
+
+    private final UserRepository  userRepository;
+
+    @PostConstruct
+    public void init() {
+        LOGGER.info("Initializing CustomUserDetailsService");
+    }
 
     @Override
     @Transactional(readOnly = true)
